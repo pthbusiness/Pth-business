fbf337b6f649885089ea537f6e1a52cab228d39b
zrywxt <sarthak6410@gmail.com>
2026-03-31 18:10:23 +0530

    Improvements made, advancements in some ideas

--- common.js
+++ common.js
@@ -1,38 +1,51 @@
 let products = [
     {
     name : "hellasd",
-    img : "4k Desktop Wallpapers Pc.jpg",
+    img : "STScI-01EVT7X0BR54ZWDP1AG2DA54RA.jpg",
     des : "lorem ispum dolor sit ametsectutur constecutor adipiscising elit."
     },
     {
     name : "xsdjlkvdn",
-    img : "4k Desktop Wallpapers Pc.jpg",
+    img : "STScI-01EVT7X0BR54ZWDP1AG2DA54RA.jpg",
     des : "arebhk  ih ihgiu wuqier hgihdakg oi hi djo khgioad h."
     },
     {
     name : "xsdjlkvdn",
-    img : "4k Desktop Wallpapers Pc.jpg",
+    img : "STScI-01EVT7X0BR54ZWDP1AG2DA54RA.jpg",
     des : "arebhk  ih ihgiu wuqier hgihdakg oi hi djo khgioad h."
     },
     {
     name : "xsdjlkvdn",
-    img : "4k Desktop Wallpapers Pc.jpg",
+    img : "STScI-01EVT7X0BR54ZWDP1AG2DA54RA.jpg",
     des : "arebhk  ih ihgiu wuqier hgihdakg oi hi djo khgioad h."
     },
     {
     name : "xsdjlkvdn",
-    img : "4k Desktop Wallpapers Pc.jpg",
+    img : "STScI-01EVT7X0BR54ZWDP1AG2DA54RA.jpg",
     des : "arebhk  ih ihgiu wuqier hgihdakg oi hi djo khgioad h."
     }
     
 ]
-const card = document.querySelector(".card");
+const container = document.querySelector(".container");
 products.forEach(item =>  {
-    const con = document.createElement("span");
-    con.setAttribute("class", "con");
-    con.innerHTML = `
-        <h2> ${item.name}</h1>
+    const card = document.createElement("span");
+    card.setAttribute("class", "con");
+    card.innerHTML = `
+        <div class="img"></div>
+        <h2> ${item.name}</h2>
         <p> ${item.des}</p>
+        <style>
+            .img {
+                background: url("${item.img}");
+                height: 60%;
+                width: 60%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+                margin-left: 21%;
+                border-radius: 5px;
+            }
+        </style>
     `;
-    card.appendChild(con);
+    container.appendChild(card);
 });
